2b1901ff61bf606ec945ee05a43d0d763f84f6b0
Tech Krowd <techkrowd@gmail.com>
2016-07-28 15:02:58 +0200

    Update and rename condicional.py to ejemploAlternativas.py

--- condicional.py
+++
@@ -1,5 +0,0 @@
-edad = 15
-if edad >= 18:
-    print("Eres mayor de edad")
-else:
-    print("No eres mayor de edad")
---
+++ ejemploAlternativas.py
@@ -0,0 +1,70 @@
+# EJEMPLO IF
+"""
+numero = int(input("Introduce un numero: "))
+
+if numero > 0:
+    print("El numero es positivo")
+	print("Bien hecho!")
+
+print("Fin del programa")
+"""
+
+#EJEMPLO IF ELSE
+
+"""
+numero = float(input("Introduce un numero:"))
+
+if numero > 0:
+    print("El numero es positivo")
+else:
+    print("El numero no es positivo")
+
+print("Fin del programa")
+"""
+
+#EJEMPLO IF ELIF
+"""
+numero = int(input("Introduce un numero: "))
+
+if numero > 0:
+    print("El numero es positivo")
+elif numero == 0:
+    print("El numero es 0")
+else:
+    print("El numero es negativo")
+
+print("Fin del programa")
+"""
+
+#EJEMPLO OPERADOR TERNARIO
+"""
+numero = float(input("Introduce un numero: "))
+
+print("El numero es positivo") if numero > 0 else print("El numero no es positivo")
+
+print("Fin del programa")
+"""
+
+numero = float(input("Introduce un numero: "))
+
+resultado = True if numero > 0 else False
+
+print("Es positivo: ", resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
