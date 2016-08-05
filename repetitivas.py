55c59061e5556a6d9f735f2d3c82b3ecfa94b8a2
Tech Krowd <techkrowd@gmail.com>
2016-08-05 14:55:27 +0200

    Add files via upload

---
+++ repetitivas.py
@@ -0,0 +1,60 @@
+#ejemplo for
+"""
+print("Inicio")
+for numero in [1,2,3,4,5]:
+    print(numero)
+print("Fin for")
+"""
+
+#range(b)
+"""
+for numero in range(5):
+    print(numero)
+
+"""
+
+#range (a,b)
+"""
+for numero in range(1,6):
+    print(numero)
+"""
+
+#range(a,b,steps)
+"""
+for numero in range(0, 101, 5):
+    print(numero)
+"""
+
+#ejemplo while
+
+numero=0
+contador=0
+while numero>=0:
+    numero = int(input("Introduce un numero: "))
+    contador = contador + 1
+print("Has introducido ",contador," numeros")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
