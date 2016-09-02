8f1fb3c4db9c8c484d1d9c33099cbd6448376707
Tech Krowd <techkrowd@gmail.com>
2016-09-02 10:05:50 +0200

    Add files via upload

---
+++ ejerciciosRepetitivas.py
@@ -0,0 +1,68 @@
+#ejercicio1
+
+"""
+suma = 0
+for n in range(0,10):
+    numero = int(input("Introduce un numero"))
+    suma = suma + numero
+print("La suma es ", suma)
+"""
+
+#ejercicio2
+"""
+numero = 0
+contador = 0
+while numero >= 0:
+    numero = int (input("Introduce un numero: "))
+    if numero >= 0:
+        contador = contador + 1
+
+#contador = contador - 1
+print("Se han introducido ", contador, " numeros")
+"""
+
+#ejercicio3
+"""
+numero = int(input("Introduce un numero: "))
+
+for i in range(1, 11):
+    print(numero, " x ", i, " = ", (numero * i))
+"""
+
+#ejercicio4
+
+numero1 = int(input("Introduce el primero numero: "))
+numero2=numero1
+while numero2 <= numero1:
+    numero2 = int(input("Introduce el segundo numero: "))
+
+for n in range(numero1, numero2+1):
+    if n % 2 == 0:
+        print(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
